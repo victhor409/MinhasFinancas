b33225908c180d3b9d2a738d5e717a7e1bdde857
victor.fonseca <victor.fonseca@semace.ce.gov.br>
2023-10-21 01:36:12 -0300

    second commit

--- MinhasFinancas/src/test/java/com/jvictor/minhasFinancas/model/repository/UsuarioRepositoryTest.java
+++ MinhasFinancas/src/test/java/com/jvictor/minhasFinancas/model/repository/UsuarioRepositoryTest.java
@@ -3,8 +3,10 @@ package com.jvictor.minhasFinancas.model.repository;
 import java.util.Optional;
 
 import org.assertj.core.api.Assertions;
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase.Replace;
@@ -15,7 +17,7 @@ import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit4.SpringRunner;
 
 import com.jvictor.minhasFinancas.model.entity.Usuario;
-
+import com.jvictor.minhasFinancas.service.impl.UsuarioServiceImpl;
 
 @RunWith(SpringRunner.class)
 @ActiveProfiles("test")
@@ -25,7 +27,7 @@ public class UsuarioRepositoryTest {
 
 	@Autowired
 	public UsuarioRepository repository;
-	
+
 	@Autowired
 	public TestEntityManager entityManager;
 
@@ -43,76 +45,55 @@ public class UsuarioRepositoryTest {
 		Assertions.assertThat(result).isTrue();
 
 	}
-	
+
 	@Test
 	public void deveRetornarFalsoQuandoNaoHouverUsuarioCadastradoComEmail() {
-		
-		//cenario
-		
-		
-		//acao
+
+		// cenario
+
+		// acao
 		boolean result = repository.existsByEmail("usuario@email.com");
-		
-		//verificacao
+
+		// verificacao
 		Assertions.assertThat(result).isFalse();
-		
+
 	}
-	
-	
+
 	@Test
 	public void devePersistirUmUsuarioNaBaseDeDados() {
-		//cenario
+		// cenario
 		Usuario usuario = criarUsuario();
-		
-		//acao
+
+		// acao
 		Usuario usuarioSalvo = repository.save(usuario);
-		
+
 		Assertions.assertThat(usuarioSalvo.getId()).isNotNull();
 	}
-	
+
 	@Test
 	public void deveBuscarUsuarioPorEmail() {
-		
-		//acao
+
+		// acao
 		Optional<Usuario> result = repository.findByEmail("usuario@email.com");
-		
+
 		Assertions.assertThat(result.isPresent());
 	}
-	
-	
-public void deveRetornarVazioAoBuscarUsuarioNaBaseAoBuscarUsuarioPorEmail() {
-		
-		//cenario
+
+	public void deveRetornarVazioAoBuscarUsuarioNaBaseAoBuscarUsuarioPorEmail() {
+
+		// cenario
 		Usuario usuario = criarUsuario();
 		entityManager.persist(usuario);
-		
-		//acao
+
+		// acao
 		Optional<Usuario> result = repository.findByEmail("usuario@email.com");
-		
+
 		Assertions.assertThat(result.isPresent());
 	}
-	
-	
-	
-	
-	
-	
+
 	public static Usuario criarUsuario() {
-		
-		return Usuario.builder()
-				.nome("usuario")
-				.email("usuario@email.com")
-				.senha("senha")
-				.build();
+
+		return Usuario.builder().nome("usuario").email("usuario@email.com").senha("senha").build();
 	}
-	
-	
-	
-	
-	
-	
-	
-	
-	
 
 }
--- MinhasFinancas/src/test/java/com/jvictor/minhasFinancas/service/UsuarioServiceTest.java
+++ MinhasFinancas/src/test/java/com/jvictor/minhasFinancas/service/UsuarioServiceTest.java
@@ -1,18 +1,26 @@
 package com.jvictor.minhasFinancas.service;
 
+import java.util.Optional;
+
+import org.assertj.core.api.Assertions;
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase.Replace;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit4.SpringRunner;
 
+import com.jvictor.minhasFinancas.exception.ErroAutenticacao;
 import com.jvictor.minhasFinancas.exception.RegraNegocioException;
 import com.jvictor.minhasFinancas.model.entity.Usuario;
 import com.jvictor.minhasFinancas.model.repository.UsuarioRepository;
+import com.jvictor.minhasFinancas.service.impl.UsuarioServiceImpl;
 
 
 @RunWith(SpringRunner.class)
@@ -21,17 +29,75 @@ import com.jvictor.minhasFinancas.model.repository.UsuarioRepository;
 @AutoConfigureTestDatabase(replace = Replace.NONE)
 public class UsuarioServiceTest {
 	
-	@Autowired
-	public UsuarioService service;
 	
-	@Autowired
-	public UsuarioRepository repository;
+	 UsuarioService service;
+	
+	 @MockBean
+	 UsuarioRepository repository;
+	
+	
+	@Before
+	public void setUp() {
+		service = new UsuarioServiceImpl(repository);
+	}
+	
+	@Test
+	public void deveAutenticarUmUsuarioComSucesso() {
+		//cenario
+		String email = "email@email.com";
+		String senha = "senha";
+		
+		Usuario usuario = Usuario.builder().email(email).senha(senha).id(1l).build();
+		
+		Mockito.when(repository.findByEmail(email)).thenReturn(Optional.of(usuario));
+		
+		//acao
+		Usuario result = service.autenticar(email, senha);
+		
+		//varificacao
+		Assertions.assertThat(result).isNotNull();
+		
+	}
+	
+	@Test
+	public void deveLancarErroQuandoNaoEncontrarUsuarioCadastradoComEmalInformado() {
+		
+		//cenario
+		Mockito.when(repository.findByEmail(Mockito.anyString())).thenReturn(Optional.empty());
+		
+		//acao
+		Throwable exception = Assertions.catchThrowable(() -> service.autenticar("email@email.com", "senha"));
+		Assertions.assertThat(exception).isInstanceOf(ErroAutenticacao.class).hasMessage("Ususario nao encontrado");
+		
+		
+	}
+	
+	@Test
+	public void deveLancarErroQuandoAsenhaNaoBater() {
+		
+		String senha = "senha";
+		
+		Usuario usuario = Usuario.builder().email("email@email.com").senha(senha).build();
+		
+		Mockito.when(repository.findByEmail(Mockito.anyString())).thenReturn(Optional.of(usuario));
+		
+		//acao
+		Throwable exception = Assertions.catchThrowable(() -> service.autenticar("email@email.com", "123"));
+		Assertions.assertThat(exception).isInstanceOf(ErroAutenticacao.class).hasMessage("Senha Invalida");
+		
+	}
+	
+	
+	
+	
+	
+	
 
 	@Test(expected = Test.None.class)
 	public void deveValidarEmail() {
 		
 		//cenario
-		repository.deleteAll();
+		Mockito.when(repository.existsByEmail(Mockito.anyString())).thenReturn(false);
 		
 		//acao
 		service.validarEmail("email@email.com");
@@ -42,8 +108,7 @@ public class UsuarioServiceTest {
 	public void deveLancarErroAoValidarEmailCadastrado() {
 		
 		//cenario
-		Usuario usuario = Usuario.builder().nome("usuario").email("email@email.com").build();
-		repository.save(usuario);
+		Mockito.when(repository.existsByEmail(Mockito.anyString())).thenReturn(true);
 		
 		//acao
 		service.validarEmail("email@email.com");
